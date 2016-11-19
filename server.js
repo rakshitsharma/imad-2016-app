12fd2bd502cc102f2a48442301900bc1eef3bd08
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-19 22:09:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/articleone.html', function(req,res){
+    res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
+});
 app.get('/articleone', function(req,res){
     res.send('article one has requested. :-)');
 });
