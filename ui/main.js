5bb05e5dc60188f6ec43d1a34bead04547ef7af2
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-22 01:41:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,10 +8,10 @@ console.log('Loaded!');
 //move the image
 var marginLeft = 0;
 function moveright() {
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
 }
 var img = document.getElementById('madi');
 img.onclick = function() {
-    var interval = setInterval(moveright, 10);
+    var interval = setInterval(moveright, 100);
 };
