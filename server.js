c4332d2f475ade6d36a62987a4d048c9e83f61b0
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-26 02:53:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.post('/login', function (req, res) {
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err, result) {
+    pool.query('SELECT * FROM "user" WHERE username=$1', [username], function(err, result) {
         if(err) {
             res.status(500).send(err.toString());
         } else {
@@ -128,22 +128,28 @@ app.post('/login', function (req, res) {
 
 app.get('/check-login', function(req, res){
     if(req.session && req.session.auth && req.session.auth.userId) {
-        res.send('You are logged in: ' + req.session.auth.userId.toString());
-    } else {
-        res.send("you are not logged in.");
-    }
+        pool.query('SELECT * FROM "user" WHERE id = $1', [req.session.auth.userId], function (err, result) {
+           if (err) {
+              res.status(500).send(err.toString());
+           } else {
+              res.send(result.rows[0].username);    
+           }
+       });
+   } else {
+       res.status(400).send('You are not logged in');
+   }
 });
 
 app.get('/logout', function(req, res){
     delete req.session.auth;
-    res.send('Logged out');
+    res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
 
 var pool = new Pool(config);
-app.get('/test-db', function(req, res){
+app.get('/get-articles', function(req, res){
    //make a select request
    //return a response with the results
-   pool.query('SELECT * FROM test', function(err, result){
+   pool.query('SELECT * FROM article ORDER BY date DESC', function(err, result){
         if(err) {
             res.status(500).send(err.toString());
         } else {
@@ -152,15 +158,47 @@ app.get('/test-db', function(req, res){
    });
 });
 
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 
-var names = [];
-app.get('/submit-name', function(req, res){  //url /submit-name?name=****  using query instead params
-   //get the name from the request
-   var name = req.query.name;
-   
-   names.push(name);
-   //JSON javascript object notation
-   res.send(JSON.stringify(names));   //converts names fron array to strings
+app.post('/submit-comment/:articleName', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                } else {
+                    var articleId = result.rows[0].id;
+                    // Now insert the right comment for this article
+                    pool.query(
+                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        [req.body.comment, articleId, req.session.auth.userId],
+                        function (err, result) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                res.status(200).send('Comment inserted!');
+                            }
+                        });
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
 });
 
 
@@ -170,7 +208,7 @@ app.get('/articles/:articleName', function(req,res){
     // articles[articleNmae] == {} content object for article one
     
     //SELECT * FROM article WHERE title = '/' ; DELETE WHERE a=/'asdf'
-    pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'" , function(err,result){
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function(err,result){
        if(err) {
            res.status(500).send(err.toString());
        } else {
@@ -186,20 +224,8 @@ app.get('/articles/:articleName', function(req,res){
 });
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/rak.jpg', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'rak.jpg'));
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 
