a86a73039101c982670a2dd71268854fce88ed85
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-26 00:31:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,8 +63,8 @@ function hash(input) {
     return hashed.toString('hex');
 }
 app.get('/hash/:input', function (req, res) {
-    var hashedString = hash(req.params.input, 'this-is-random-string');
-    res.send(hashedSString);
+    var hashedString = hash(req.params.input, 'this-is-some-random-string');
+    res.send(hashedString);
 });
 
 
