8f809de0349a6eee1113d4f2ec90990d188ba22c
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-26 01:41:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-//submit name
+//submit username/password
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -7,25 +7,25 @@ submit.onclick = function() {
     var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.status === 200) {
-                     
-            //capture a list of names and render it as a list
-            var names = request.responseText;
-            names = JSON.parse(names);  //to convert names from string to array
-            var list = '';
-            for( var i=0 ; i<names.length ; i++){
-                list += '<li>' + names[i] + '</li>';
+    request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+                if (request.status === 200) {
+                    alert("Logged in successfully");
+                } else if(request.status === 403) {
+                    alert("Username/password is incorrect");
+                } else if(request.status === 500) {                    
+                    alert("Something went wrong on the server");
+                }
             }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list ;
-        }  
-        //not done yet
     };
     //make a request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('Get', 'http://rakshitsharma.imad.hasura-app.io/submit-name?name=' + name , true);
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    Request.open('POST', '/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
 }; 
