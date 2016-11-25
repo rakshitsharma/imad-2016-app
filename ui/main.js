c5e0d98949bd5580fd3b3e6f1d94af27dc193a2d
rakshitsharma <rakshit.jangra123@gmail.com>
2016-11-25 19:11:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,25 +1,3 @@
-//counter code
-var button = document.getElementById('counter');    
-
-button.onclick = function(){
-    
-    //create a request
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if(request.status === 200) {
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-        }  
-        //not done yet
-    };
-    //make a request
-    request.open('Get', 'http://rakshitsharma.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
-
 //submit name
 
 var submit = document.getElementById('submit_btn');
